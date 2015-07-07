72f481e7c51b2752aab55ba7ac051569db7aee05
greenjm <greenjm@rose-hulman.edu>
2015-07-07 11:54:27 -0400

    backup and deleting old backups functioning
    Sending email on exception still not working

--- SQLBackupTool/SQLBackupTool/Program.cs
+++ SQLBackupTool/SQLBackupTool/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
-
+using System.Net.Mail;
 using System.Data.SqlClient;
 
 namespace SQLBackupTool
@@ -13,37 +13,72 @@ namespace SQLBackupTool
 	{
 		static void Main(string[] args)
 		{
-			//SqlConnection conn = new SqlConnection("Data Source=(LocalDb)\v11.0;Initial Catalog=ContosoUniversity2;Integrated Security=SSPI;");
-			//conn.Open();
-			//SqlCommand cmd = new SqlCommand("show tables", conn);
-			//cmd.ExecuteNonQuery();
+			Dictionary<String, String> parameters = getConfigParameters();
 
-			ProcessStartInfo cmdStartInfo = new ProcessStartInfo();
-			cmdStartInfo.FileName = @"C:\Windows\System32\cmd.exe";
-			cmdStartInfo.RedirectStandardOutput = true;
-			cmdStartInfo.RedirectStandardError = true;
-			cmdStartInfo.RedirectStandardInput = true;
-			cmdStartInfo.UseShellExecute = false;
-			cmdStartInfo.CreateNoWindow = true;
+			String connectionString = String.Format(@"Data Source={0};Initial Catalog={1};Persist Security Info=True;User ID={2};Password={3}",
+				parameters["serverName"], parameters["dbName"], parameters["userID"], parameters["password"]);
 
-			Process p = new Process();
-			p.StartInfo = cmdStartInfo;
-			p.OutputDataReceived += runCommand;
-			p.Start();
-			p.BeginOutputReadLine();
-			p.BeginErrorReadLine();
-			string[] commands = System.IO.File.ReadAllLines(@"./../../TextFile1.bat");
-			foreach (string command in commands)
+			SqlConnection conn = new SqlConnection(@"" + parameters["connectionString"]);
+			conn.Open();
+			SqlCommand cmd = new SqlCommand("BACKUP DATABASE " + parameters["dbName"] + " TO DISK='" + parameters["backupPath"] + parameters["dbName"] + "-" + DateTime.UtcNow.ToString("yyyy-MM-dd-HH-mm-ss") + "-.bak'", conn);
+			try 
 			{
-				p.StandardInput.WriteLine(command);
+				cmd.ExecuteNonQuery();
+				string[] backupFiles = System.IO.Directory.GetFiles(parameters["backupPath"], parameters["dbName"] + "*");
+				int countBackups = backupFiles.Length;
+				if (countBackups > Int32.Parse(parameters["maxBackups"]))
+				{
+					removeOldestBackup(backupFiles, parameters);
+				}
 			}
-			//p.StandardInput.WriteLine("Testing...");
-			p.WaitForExit();
+			catch (Exception e)
+			{
+				MailMessage mail = new MailMessage();
+				mail.From = new MailAddress("joshue.green@wddsoftware.com");
+				mail.To.Add(new MailAddress("joshua.green@wddsoftware.com"));
+				SmtpClient client = new SmtpClient();
+				client.Port = 587;
+				client.EnableSsl = true;
+				client.DeliveryMethod = SmtpDeliveryMethod.Network;
+				client.UseDefaultCredentials = false;
+				client.Host = "smtp.gmail.com";
+				mail.Subject = "Error with Database Backup";
+				mail.Body = "There was an error backing up " + parameters["dbName"] + "\n" + e.Message;
+				client.Send(mail);
+				throw;
+			}
+			
 		}
 
-		static void runCommand(object sender, DataReceivedEventArgs e)
+		private static void removeOldestBackup(string[] backupFiles, Dictionary<String, String> parameters)
 		{
-			Console.WriteLine(e.Data);
+			DateTime[] timeStamps = new DateTime[backupFiles.Length];
+			for (int i = 0; i < backupFiles.Length; i++)
+			{
+				string[] timestamp = backupFiles[i].Split('-');
+				DateTime toAdd = new DateTime(Int32.Parse(timestamp[1]), Int32.Parse(timestamp[2]), Int32.Parse(timestamp[3]),
+					Int32.Parse(timestamp[4]), Int32.Parse(timestamp[5]), Int32.Parse(timestamp[6]));
+				timeStamps[i] = toAdd;
+			}
+
+			DateTime leastRecentBackup = timeStamps.OrderBy(x => x).FirstOrDefault();
+			string filePath = parameters["backupPath"] + parameters["dbName"] + "-" + leastRecentBackup.ToString("yyyy-MM-dd-HH-mm-ss") + "-.bak";
+			System.IO.File.Delete(filePath);
+		}
+
+		static Dictionary<String, String> getConfigParameters()
+		{
+			Dictionary<String, String> result = new Dictionary<String, String>();
+			using (System.IO.StreamReader sr = new System.IO.StreamReader("./../../config.txt"))
+			{
+				while (!sr.EndOfStream)
+				{
+					String nextLine = sr.ReadLine();
+					string[] split = nextLine.Split(new string[] { "=>" }, StringSplitOptions.None);
+					result.Add(split[0].Trim(), split[1].Trim());
+				}
+			}
+			return result;
 		}
 	}
 }
--- SQLBackupTool/SQLBackupTool/TextFile1.bat
+++
@@ -1,1 +0,0 @@
-﻿"C:\Program Files\Microsoft SQL Server\120\Tools\Binn\OSQL.exe" -E -q "BACKUP DATABASE master TO DISK='C:\Temp\db.bak' WITH FORMAT"
